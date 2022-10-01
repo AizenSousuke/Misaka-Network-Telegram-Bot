1e3fbf4dcda63b4bb4038ce9a07f5f96e8d26888
Nik <puteranda_nik@certisgroup.com>
2022-10-01 22:28:58 +0800

    Added some samples

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
 
-const TOKEN = process.env.TOKEN;
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const OPEN_AI_KEY = process.env.OPEN_AI_KEY;
 
-const BOT = new TelegramBot(TOKEN, {
+const BOT = new TelegramBot(TELEGRAM_BOT_TOKEN, {
 	polling: true,
 });
 
-BOT.on("message", (message) => {
+BOT.on("polling_error", console.log);
+
+BOT.onText(/Hello World/, (message) => {
     const chatId = message.chat.id;
-    BOT.sendMessage(chatId, "Misaka has received your message :3");
+    BOT.sendAnimation(chatId, "https://media3.giphy.com/media/PnUatAYWMEMvmiwsyx/giphy.gif?cid=ecf05e471tflyileji3sdwd6dnxd5gzbo7qeofk139j497dj&rid=giphy.gif&ct=g");
 });
+
+BOT.onText(/\\echo (.*)/, (message) => {
+    const chatId = message.chat.id;
+    BOT.sendMessage(chatId, "Echoing " + message.text.replace("\\echo ", "") + " back to you :D");
+});
+
+// BOT.on("message", (message) => {
+//     const chatId = message.chat.id;
+//     switch (message.text) {
+//         case "Testing":
+//             BOT.sendMessage(chatId, "Misaka has received your message :3");
+//             break;
+//         default:
+//             BOT.sendMessage(chatId, "...");
+//             break;
+//     }
+// });
