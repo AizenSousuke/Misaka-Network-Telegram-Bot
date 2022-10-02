f0cb5eb7e16d7e0701a42f30b1e925143cfee70c
Nik <puteranda_nik@certisgroup.com>
2022-10-02 11:20:19 +0800

    Added Open AI

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const TelegramBot = require("node-telegram-bot-api");
+const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const OPEN_AI_KEY = process.env.OPEN_AI_KEY;
+const OPEN_AI_ORGANIZATION_ID = process.env.OPEN_AI_ORGANIZATION_ID;
+const OPEN_AI_CONFIGURATION = new Configuration({
+    organization: OPEN_AI_ORGANIZATION_ID,
+    apiKey: OPEN_AI_KEY
+});
+const openai = new OpenAIApi(OPEN_AI_CONFIGURATION);
 
 const BOT = new TelegramBot(TELEGRAM_BOT_TOKEN, {
 	polling: true,
@@ -10,14 +17,29 @@ const BOT = new TelegramBot(TELEGRAM_BOT_TOKEN, {
 
 BOT.on("polling_error", console.log);
 
-BOT.onText(/Hello World/, (message) => {
-    const chatId = message.chat.id;
-    BOT.sendAnimation(chatId, "https://media3.giphy.com/media/PnUatAYWMEMvmiwsyx/giphy.gif?cid=ecf05e471tflyileji3sdwd6dnxd5gzbo7qeofk139j497dj&rid=giphy.gif&ct=g");
+BOT.onText(/\/datetime/, (message) => {
+	const chatId = message.chat.id;
+	const date = new Date().toLocaleString("en-SG", {
+		timeZone: "Asia/Singapore",
+		hour12: true,
+	});
+	BOT.sendMessage(chatId, date);
 });
 
-BOT.onText(/\\echo (.*)/, (message) => {
-    const chatId = message.chat.id;
-    BOT.sendMessage(chatId, "Echoing " + message.text.replace("\\echo ", "") + " back to you :D");
+BOT.onText(/Hello World/, (message) => {
+	const chatId = message.chat.id;
+	BOT.sendAnimation(
+		chatId,
+		"https://media3.giphy.com/media/PnUatAYWMEMvmiwsyx/giphy.gif?cid=ecf05e471tflyileji3sdwd6dnxd5gzbo7qeofk139j497dj&rid=giphy.gif&ct=g"
+	);
+});
+
+BOT.onText(/\/echo (.*)/, (message) => {
+	const chatId = message.chat.id;
+	BOT.sendMessage(
+		chatId,
+		"Echoing " + message.text.replace("\/echo ", "") + " back to you :D"
+	);
 });
 
 // BOT.on("message", (message) => {
