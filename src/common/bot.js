b826d1b69a6643107efcdf8a15f7c5a2a8468c77
AizenSousuke <puteranikdimas@yahoo.com.sg>
2023-07-13 22:37:23 +0800

    Changed some code

--- src/common/bot.js
+++ src/common/bot.js
@@ -16,69 +16,87 @@ export const TELEGRAM_BOT = () => {
 		ctx.reply("Echo back");
 	});
 
-	console.log("Finished running bot");
+	// SETUP_TELEGRAM_BOT(BOT);
+
+	console.log("Finished creating bot.");
 
 	return BOT;
 };
 
 export const SETUP_TELEGRAM_BOT = async (BOT) => {
-	const openai = OPEN_AI();
+	// const openai = OPEN_AI();
 
-	BOT.onText(/\/start/, (message) => {
-		const chatId = message.chat.id;
+	// BOT.onText(/\/start/, (message) => {
+	// 	const chatId = message.chat.id;
 
-		BOT.sendMessage(
-			chatId,
-			`
-	    <b>Welcome to the Misaka Network.</b>
+	// 	BOT.sendMessage(
+	// 		chatId,
+	// 		`
+	//     <b>Welcome to the Misaka Network.</b>
 
-	    This Telegram Bot has an Open AI functionality.
+	//     This Telegram Bot has an Open AI functionality.
 
-	    To get started, please choose type one of the following commands:
-	    /start - show this message
-	    /complete &lt;text&gt; - use Open AI to complete a sentence or write something out
-	    /datetime - get the current date and time in Singapore
-	    `,
-			{ parse_mode: "HTML" }
-		);
-	});
+	//     To get started, please choose type one of the following commands:
+	//     /start - show this message
+	//     /complete &lt;text&gt; - use Open AI to complete a sentence or write something out
+	//     /datetime - get the current date and time in Singapore
+	//     `,
+	// 		{ parse_mode: "HTML" }
+	// 	);
+	// });
 
-	BOT.onText(/\/datetime/, (message) => {
-		const chatId = message.chat.id;
-		const date = new Date().toLocaleString("en-SG", {
-			timeZone: "Asia/Singapore",
-			hour12: true,
-		});
-		BOT.sendMessage(chatId, date);
-	});
+	// BOT.onText(/\/datetime/, (message) => {
+	// 	const chatId = message.chat.id;
+	// 	const date = new Date().toLocaleString("en-SG", {
+	// 		timeZone: "Asia/Singapore",
+	// 		hour12: true,
+	// 	});
+	// 	BOT.sendMessage(chatId, date);
+	// });
 
-	BOT.onText(/Hello World/, (message) => {
-		const chatId = message.chat.id;
-		BOT.sendAnimation(
-			chatId,
-			"https://media3.giphy.com/media/PnUatAYWMEMvmiwsyx/giphy.gif?cid=ecf05e471tflyileji3sdwd6dnxd5gzbo7qeofk139j497dj&rid=giphy.gif&ct=g"
-		);
-	});
+	// BOT.onText(/Hello World/, (message) => {
+	// 	const chatId = message.chat.id;
+	// 	BOT.sendAnimation(
+	// 		chatId,
+	// 		"https://media3.giphy.com/media/PnUatAYWMEMvmiwsyx/giphy.gif?cid=ecf05e471tflyileji3sdwd6dnxd5gzbo7qeofk139j497dj&rid=giphy.gif&ct=g"
+	// 	);
+	// });
 
-	BOT.onText(/\/echo (.*)/, (message) => {
-		const chatId = message.chat.id;
-		BOT.sendMessage(
-			chatId,
-			"Echoing " + RemoveCommand(message.text) + " back to you :D"
-		);
-	});
+	// BOT.onText(/\/echo (.*)/, (message) => {
+	// 	const chatId = message.chat.id;
+	// 	BOT.sendMessage(
+	// 		chatId,
+	// 		"Echoing " + RemoveCommand(message.text) + " back to you :D"
+	// 	);
+	// });
 
-	BOT.onText(/\/complete (.*)/, async (message) => {
-		const chatId = message.chat.id;
-		const prompt = RemoveCommand(message.text);
+	// BOT.onText(/\/complete (.*)/, async (message) => {
+	// 	const chatId = message.chat.id;
+	// 	const prompt = RemoveCommand(message.text);
 
-		const response = await openai.createCompletion(
-			completion.CompletionModel({ prompt: prompt })
-		);
+	// 	const response = await openai.createCompletion(
+	// 		completion.CompletionModel({ prompt: prompt })
+	// 	);
 
-		if (response) {
-			BOT.sendMessage(chatId, response.data.choices[0].text);
-		}
+	// 	if (response) {
+	// 		BOT.sendMessage(chatId, response.data.choices[0].text);
+	// 	}
+	// });
+
+	BOT.on('message', async (ctx) => {
+		ctx.reply("Test");
+		// const chatId = message.chat.id;
+		// const prompt = RemoveCommand(message.text);
+
+		// const response = await openai.createCompletion(
+		// 	completion.CompletionModel({ prompt: prompt })
+		// );
+
+		// if (response) {
+		// 	ctx.reply(chatId, response.data.choices[0].text);
+		// } else {
+		// 	ctx.reply(chatId, "No response from OPENAI.");
+		// }
 	});
 };
 
